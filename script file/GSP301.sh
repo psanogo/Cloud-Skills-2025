56f78cf04c8de2ec32fa35efebf3c08092ef7581
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-04-15 12:50:15 +0530

    Update GSP301.sh

--- script file/GSP301.sh
+++ script file/GSP301.sh
@@ -1,52 +1,69 @@
 #!/bin/bash
 
-# Define text colors and formatting
-BLACK_TEXT=$(tput setaf 0)
-RED_TEXT=$(tput setaf 1)
-GREEN_TEXT=$(tput setaf 10) # Brighter Green
-YELLOW_TEXT=$(tput setaf 3)
-BLUE_TEXT=$(tput setaf 4)
-MAGENTA_TEXT=$(tput setaf 5)
-CYAN_TEXT=$(tput setaf 6)
-WHITE_TEXT=$(tput setaf 7)
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
 
-RESET_FORMAT=$(tput sgr0)
-BOLD_TEXT=$(tput bold)
-ITALIC_TEXT=$(tput sitm)
-UNDERLINE_TEXT=$(tput smul)
-clear # Clear the terminal screen
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
 
-# --- Script Header ---
-echo
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
+# Welcome message
 echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
-echo "${BLUE_TEXT}${BOLD_TEXT}         STARTING EXECUTION...       ${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
 echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
 echo
 
-echo -n "${YELLOW_TEXT}${BOLD_TEXT}Please enter the zone: ${RESET_FORMAT}"
-read ZONE
+read -p "$(echo -e ${MAGENTA_TEXT}${BOLD_TEXT}Enter the zone:${RESET_FORMAT} ) " ZONE
 export ZONE
 
-# Enable the required API
-echo "${GREEN_TEXT}${BOLD_TEXT}Enabling the Filestore API...${RESET_FORMAT}"
-gcloud services enable file.googleapis.com
+# Informing the user about the next steps
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Creating a storage bucket in your project...${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}This bucket will store the startup script.${RESET_FORMAT}"
+echo
 
-# Create a Compute Engine instance
-echo "${GREEN_TEXT}${BOLD_TEXT}Creating a Compute Engine instance named 'nfs-client'...${RESET_FORMAT}"
-gcloud compute instances create nfs-client \
---project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium \
---network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
---metadata=enable-oslogin=true \
---maintenance-policy=MIGRATE \
---provisioning-model=STANDARD \
---scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server \
---create-disk=auto-delete=yes,boot=yes,device-name=nfs-client,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231010,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+gsutil mb gs://$DEVSHELL_PROJECT_ID
 
-# Create a Filestore instance
-echo "${GREEN_TEXT}${BOLD_TEXT}Creating a Filestore instance named 'nfs-server'...${RESET_FORMAT}"
-gcloud filestore instances create nfs-server \
---zone=$ZONE --tier=BASIC_HDD \
---file-share=name="vol1",capacity=1TB \
---network=name="default"
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Copying the startup script to the storage bucket...${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}The script will be used to configure the VM instance.${RESET_FORMAT}"
+echo
 
+gsutil cp gs://sureskills-ql/challenge-labs/ch01-startup-script/install-web.sh gs://$DEVSHELL_PROJECT_ID
 
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Creating a Compute Engine instance...${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}This instance will run the startup script to set up a web server.${RESET_FORMAT}"
+echo
+
+gcloud compute instances create quickgcplab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=n1-standard-1 --tags=http-server --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID/install-web.sh
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Setting up a firewall rule to allow HTTP traffic...${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}This will enable access to the web server on port 80.${RESET_FORMAT}"
+echo
+
+gcloud compute firewall-rules create allow-http \
+    --allow=tcp:80 \
+    --description="awesome lab" \
+    --direction=INGRESS \
+    --target-tags=http-server
+
+# Completion message
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
