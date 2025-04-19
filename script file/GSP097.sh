da9023d4512a9d8047d7e87c95c858f45deefd30
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-04-19 11:37:34 +0530

    Create GSP097.sh

---
+++ script file/GSP097.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define color variables
+YELLOW_TEXT=$'\033[0;33m'
+MAGENTA_TEXT=$'\033[0;35m'
+NO_COLOR=$'\033[0m'
+GREEN_TEXT=$'\033[0;32m'
+RED_TEXT=$'\033[0;31m'
+CYAN_TEXT=$'\033[0;36m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
+BLUE_TEXT=$'\033[0;34m'
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}Starting the process...${RESET_FORMAT}"
+echo
+
+# Displaying instructions
+echo "${YELLOW_TEXT}${BOLD_TEXT}Fetching the Compute Engine instance zone...${RESET_FORMAT}"
+ZONE="$(gcloud compute instances list --project=$DEVSHELL_PROJECT_ID --format='value(ZONE)')"
+
+
+export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value core/project)
+gcloud iam service-accounts create my-natlang-sa \
+  --display-name "my natural language service account"
+
+gcloud iam service-accounts keys create ~/key.json \
+  --iam-account my-natlang-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+
+export GOOGLE_APPLICATION_CREDENTIALS="/home/USER/key.json"
+
+sleep 10
+
+gcloud compute ssh --zone "$ZONE" "linux-instance" --project "$DEVSHELL_PROJECT_ID" --quiet --command "gcloud ml language analyze-entities --content='Michelangelo Caravaggio, Italian painter, is known for \"The Calling of Saint Matthew\".' > result.json"
+echo
+
+# Safely delete the script if it exists
+SCRIPT_NAME="arcadecrew.sh"
+if [ -f "$SCRIPT_NAME" ]; then
+    echo -e "${BOLD_TEXT}${RED_TEXT}Deleting the script ($SCRIPT_NAME) for safety purposes...${RESET_FORMAT}${NO_COLOR}"
+    rm -- "$SCRIPT_NAME"
+fi
+
+echo
+# Completion message
+echo -e "${MAGENTA_TEXT}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
