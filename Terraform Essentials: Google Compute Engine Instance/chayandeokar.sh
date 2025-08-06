3dddd39ad233e78ce04536f0460d0e9414398653
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2025-08-06 21:40:59 +0530

    Create chayandeokar.sh

---
+++ Terraform Essentials: Google Compute Engine Instance/chayandeokar.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Fetch zone and region
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+
+
+terraform version
+
+gcloud version
+
+gcloud auth login
+
+gcloud config set project $PROJECT_ID
+
+gsutil mb -l "REGION" gs://$PROJECT_ID-tf-state
+
+gsutil versioning set on gs://$PROJECT_ID-tf-state
+
+
+cat > main.tf <<EOF_END
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  backend "gcs" {
+    bucket = ""PROJECT_ID"-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {
+    }
+  }
+}
+EOF_END
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project"
+  default = ""PROJECT_ID""
+}
+
+variable "region" {
+  type        = string
+  description = "The region to deploy resources in"
+  default     = ""REGION""
+}
+
+variable "zone" {
+  type        = string
+  description = "The zone to deploy resources in"
+  default     = ""ZONE""
+}
+EOF_END
+
+
+terraform init
+
+terraform plan
+
+terraform apply -auto-approve
+
+gcloud compute instances list
+
+
+
+
